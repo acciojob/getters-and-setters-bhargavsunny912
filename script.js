d4ebe62e9f7e7ad20e874d3337bf0acb67e94dda
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-11-23 12:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 //complete this code
 class Person {
-  #name;
-  #age;
-
   constructor(name, age) {
-    this.#name = name;
-    this.#age = age;
+    this._name = name;
+    this._age = age;
   }
 
   get name() {
-    return this.#name;
+    return this._name;
   }
 
   set name(name) {
-    this.#name = name;
+    this._name = name;
   }
 
   get age() {
-    return this.#age;
+    return this._age;
   }
 
   set age(age) {
-    this.#age = age;
+    this._age = age;
   }
 }
 
