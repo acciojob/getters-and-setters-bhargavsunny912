8160f01560b48b5fcad513ef141979d85dd58ccb
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-11-23 12:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
+  #name;
+  #age;
+
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(name) {
+    this.#name = name;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  set age(age) {
+    this.#age = age;
+  }
 }
 
 class Student extends Person {
-	constructor(name,age){
-		super(name,age);
-	}
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+  constructor(name, age) {
+    super(name, age);
+  }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	constructor(name,age){
-		super(name,age);
-	}
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  constructor(name, age) {
+    super(name, age);
+  }
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
-let p1=new Person("john",25);
+let p1 = new Person("john", 25);
 console.log(p1.name);
-p1.age=30;
+p1.age = 30;
 console.log(p1.age);
 
-let s1=new Student("john",30);
+let s1 = new Student("john", 30);
 s1.study();
 
-let T1=new Teacher("john",30);
-T1.teach()
+let T1 = new Teacher("john", 30);
+T1.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
